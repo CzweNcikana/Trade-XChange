c3f53c232eddae40e7fee3ab78226f08d0efe725
Sizwell <sizwe.ncikana@flash.co.za>
2024-06-23 23:05:10 +0200

    Code clean up

--- XChange-service/src/main/java/com/itec/xchangeservice/controller/XChangeController.java
+++ XChange-service/src/main/java/com/itec/xchangeservice/controller/XChangeController.java
@@ -1,13 +1,13 @@
 package com.itec.xchangeservice.controller;
 
-import com.itec.xchangeservice.dto.XChangeRateDTO;
-import com.itec.xchangeservice.entity.XChangeEntity;
 import com.itec.xchangeservice.service.XChangeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/v1")
